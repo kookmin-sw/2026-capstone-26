548a0685fedf220f3bfe0b04dd2f35db66196aae
zie-ning <jeeun03@gmail.com>
2026-05-08 10:44:05 +0900

    docs #27: 스웨거 응답, 요청 예시 추가

--- backend/src/main/java/backend/capstone/domain/friend/friendinvite/dto/FriendInviteLinkResponse.java
+++ backend/src/main/java/backend/capstone/domain/friend/friendinvite/dto/FriendInviteLinkResponse.java
@@ -1,9 +1,11 @@
 package backend.capstone.domain.friend.friendinvite.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record FriendInviteLinkResponse(
+    @Schema(example = "https://passedpath.site/friend/invite?inviteCode=T5rfCFFy9j")
     String inviteLink
 ) {
 
