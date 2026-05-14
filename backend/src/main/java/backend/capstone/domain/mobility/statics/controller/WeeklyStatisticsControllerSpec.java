676671dc66eda4e8e9aca15da2896310a2f963c2
zie-ning <jeeun03@gmail.com>
2026-05-14 14:38:16 +0900

    chore #38: 통계 -> 요약

--- backend/src/main/java/backend/capstone/domain/mobility/statics/controller/WeeklyStatisticsControllerSpec.java
+++ backend/src/main/java/backend/capstone/domain/mobility/statics/controller/WeeklyStatisticsControllerSpec.java
@@ -5,11 +5,11 @@ import backend.capstone.domain.mobility.statics.dto.WeeklyStatisticsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "주간 통계 API")
+@Tag(name = "주간 요약 API")
 public interface WeeklyStatisticsControllerSpec {
 
     @Operation(
-        summary = "최근 7일 주간 통계 조회 API",
+        summary = "최근 7일 주간 요약 조회 API",
         description = """
             오늘 날짜를 기준으로 최근 7일간의 통계가 날짜 오름차순으로 반환합니다.<br>
             최근 7일간 생성된 dayRoute가 없다면 평균을 계산할 수치가 없어 각 필드의 `average`의 `value`와 `displayText`는 nulll이 됩니다. (이때 nulll이 아니라 0이나 "-" 같은 문자열을 받고 싶으면 얘기 바람)
