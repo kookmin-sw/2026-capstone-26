d696dfe732a9ca0e076a4bb02c1d6ba24d2d8da0
zie-ning <jeeun03@gmail.com>
2026-05-29 11:54:53 +0900

    feat: general 엑세스토큰 발급 api로 변경

--- backend/src/main/java/backend/capstone/auth/controller/AuthController.java
+++ backend/src/main/java/backend/capstone/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -51,10 +52,9 @@ public class AuthController implements AuthControllerSpec {
         return authService.testIssue(2L);
     }
 
-    @Override
-    @GetMapping("/user3-token/issue")
-    public TokenPair issueUser3Jwt() {
-        return authService.testIssue(3L);
+    @GetMapping("/general-user-token/issue")
+    public TokenPair issueGeneralUserJwt(@PathVariable("userId") Long userId) {
+        return authService.testIssue(userId);
     }
 
     private String resolveBearerToken(HttpServletRequest request) {
